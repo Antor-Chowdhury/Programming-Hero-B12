055218edaa8642ed1ff35fb0e67b5fd7b3d772ae
Antor-Chowdhury <antor1347@gmail.com>
2025-07-26 22:21:36 +0600

    added naming.js in module-13

---
+++ Milestone-3/Module-13/Introduction to JavaScript/naming.js
@@ -0,0 +1,23 @@
+// 1. no keyword in variable name
+var flase = 45;
+var var = 50;
+
+// 2. no space or gap in variable name
+var is happy = false;
+
+// 3. no quote
+var "address" = "dhaka";
+
+// 4. can not start with a number
+var 3money = 50;
+
+// 5. name is case sensitive
+address
+Address
+ADDRESS
+
+// 6. how to write a long variable name
+
+var my_Current_Home_Address = 'Sirajgonj'; // Snake Case
+var myCurrentHomeAddress = 'Sirajgonj'; // Camel Case > used in JS 
+var MyCurrentHomeAddress = 'Sirajgonj'; // Pascal Case
