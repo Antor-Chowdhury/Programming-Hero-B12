3d36579cbb09db1213c12d7538caebee015c4917
Antor-Chowdhury <antor1347@gmail.com>
2025-07-26 23:02:19 +0600

    added number.js in module-13

---
+++ Milestone-3/Module-13/Introduction to JavaScript/number.js
@@ -0,0 +1,24 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+// var applePrice = '26';
+// var applePrice = parseInt('20');
+var applePrice = parseFloat('20.2');
+// var applePrice = Number('24');
+
+// console.log(orangePrice);
+// console.log(typeof(orangePrice));
+// console.log(typeof(applePrice));
+
+// console.log(orangePrice + applePrice);
+
+// var price = parseInt('asdfdafafdadfa');
+// console.log(price);
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+console.log(total);
+console.log(total.toFixed(3));
+
+const num = 50 / 0;
+console.log(num);
