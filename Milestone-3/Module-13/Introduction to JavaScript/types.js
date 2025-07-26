12a469b70fe0604ebe125319bb67145227a606e4
Antor-Chowdhury <antor1347@gmail.com>
2025-07-26 21:54:34 +0600

    added types.js in module-13

---
+++ Milestone-3/Module-13/Introduction to JavaScript/types.js
@@ -0,0 +1,22 @@
+// String Type Variable
+var name = "Antor Chowdhury";
+// console.log(name)
+
+var address = "Sirajgonj";
+
+var country = 'Bangladesh';
+
+
+// Boolean type variable
+var isRich = true;
+console.log(isRich);
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed);
+
+console.log(typeof country);
+
+// Number/Numeric type variable 
+var price = 500;
+console.log(typeof price);
