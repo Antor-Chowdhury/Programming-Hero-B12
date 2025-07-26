2d6cbdbaf9b8d1bcd5886387cace02d4f1d6e85c
Antor-Chowdhury <antor1347@gmail.com>
2025-07-27 01:01:23 +0600

    added mixed.js in module-13

---
+++ Milestone-3/Module-13/Introduction to JavaScript/mixed.js
@@ -0,0 +1,15 @@
+var first = 'Monkey D.';
+var second = 'Luffy';
+var fullname = first + second;
+console.log(fullname);
+
+console.log(40 + '2');
+console.log(40 - '2');
+console.log(40 * '2');
+console.log(40 / '2');
+
+const a = isNaN(9);
+console.log(a);
+
+const b = isNaN(4 - 'hello');
+console.log(b);
