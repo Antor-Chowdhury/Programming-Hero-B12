932c54db20e0b673aca67b44a931e04f98f62f9a
Antor-Chowdhury <antor1347@gmail.com>
2025-07-27 01:08:19 +0600

    added practice_1.js in module-13

---
+++ Milestone-3/Module-13/Introduction to JavaScript/practice_1.js
@@ -0,0 +1,9 @@
+var moneyIHave = 1000;
+
+var costOf1kgOranges = 400;
+var costOf1kgApples = 300;
+
+var totalCost = costOf1kgApples + costOf1kgOranges;
+
+var moneyReturned = moneyIHave - totalCost;
+console.log(moneyReturned);
