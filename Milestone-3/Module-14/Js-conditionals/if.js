36db92a92c9c9a9102a6539ea7730d6be27e7978
Antor-Chowdhury <antor1347@gmail.com>
2025-07-27 22:22:02 +0600

    added if.js in module-14

---
+++ Milestone-3/Module-14/Js-conditionals/if.js
@@ -0,0 +1,35 @@
+/***
+ * -----------STRUCTURE------------
+ * if (condition) {
+ *    execute code if the condition is true
+ * }
+ * else {
+ *   execute code if the condition is false
+ * }
+*/
+
+if (3 < 10) {
+    console.log("I've a smaller value.");
+}
+
+var briyaniPrice = 400;
+
+if (briyaniPrice < 300) {
+    console.log('mama give me some briyani');
+}
+
+var weight = 20;
+
+if (weight <= 20) {
+    console.log('I will carry it by myself');
+}
+else {
+    console.log('i will rent a rickshaw');
+}
+
+let a = 2;
+if (a >= 5) {
+    console.log('if statement executed');
+} else {
+    console.log('else statement is executed');
+}
