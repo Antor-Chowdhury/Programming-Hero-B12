d1aafce052067f271e69ac26f4c2b4e78cb7750e
Antor-Chowdhury <antor1347@gmail.com>
2025-07-27 22:06:26 +0600

    added comparison.js in module-14

---
+++ Milestone-3/Module-14/Js-conditionals/comparison.js
@@ -0,0 +1,33 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+
+// console.log(10 >= 10);
+// console.log(5 <= 10);
+
+// console.log(10 == 10.2);
+
+// ----------------------------- //
+
+var peryaraPrice = 40;
+var applePrice = 250;
+
+// console.log(peryaraPrice > applePrice);
+// console.log(applePrice < peryaraPrice);
+
+
+/***
+ * ==
+ * === 
+*/
+
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+// ------------------------
+
+console.log(10 != 10);
+console.log(10 !== '10');
+console.log(10 == '10');
+console.log(10 === '10');
