83224efc2064ac6622ebda1d558f9b78c3f17c53
Antor-Chowdhury <antor1347@gmail.com>
2025-07-28 00:57:28 +0600

    added nested.js in module-14

---
+++ Milestone-3/Module-14/Js-conditionals/nested.js
@@ -0,0 +1,15 @@
+const money = 50;
+
+if (money > 300) {
+    console.log('bro you are rich!!!');
+} else {
+    if (money > 100) {
+        console.log('Neither poor nor rich');
+    } else {
+        if(money > 0) {
+            console.log('You have few pennies left');
+        } else {
+            console.log('No money No honey');
+        }
+    }
+} 
