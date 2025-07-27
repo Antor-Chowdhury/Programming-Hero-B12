402644ed60a28855ce1abe3e23622b476a7b4b23
Antor-Chowdhury <antor1347@gmail.com>
2025-07-28 03:01:26 +0600

    added practice_5.js in module-14

---
+++ Milestone-3/Module-14/Js-conditionals/practice_5.js
@@ -0,0 +1,20 @@
+const num1 = 10;
+const num2 = 20;
+
+let result;
+
+// simple if-else
+
+// if (num1 > num2) {
+//     result = num1 * 2;
+//     console.log(result);
+// } else{
+//     result = num1 + num2 ;
+//     console.log(result);
+// }
+
+
+// ternary operator
+
+result = num1 > num2 ? result = num1 * 2 : result = num1 + num2;
+console.log(result)
