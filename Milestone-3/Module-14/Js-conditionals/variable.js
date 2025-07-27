b69bf3c388847f6bb76fcc2bf4c543a10d18d46b
Antor-Chowdhury <antor1347@gmail.com>
2025-07-27 21:23:45 +0600

    added variable.js in module-14

---
+++ Milestone-3/Module-14/Js-conditionals/variable.js
@@ -0,0 +1,33 @@
+/* 
+* Variable ==> container 
+*/
+
+//number 
+var sunglassPrice = 80;
+
+// string
+var subject = 'CSE';
+
+// Boolean
+var passed = true;
+
+/*
+ * NAMING CONVENTION
+ * 1. single word
+ * no quote
+ * no gap or dash
+ * can not start with a number
+ * can not be a keyword
+ * prefer camelCase
+*/
+
+var lastExamResult = 89;
+
+/**
+ * OPERATION
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN ?
+ * 4. null
+ * 5. Number
+*/
