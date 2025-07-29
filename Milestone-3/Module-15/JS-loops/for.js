a88eca3208f1060ad3ecd57c445e738e73a05635
Antor-Chowdhury <antor1347@gmail.com>
2025-07-29 22:59:04 +0600

    added for.js in module-15

---
+++ Milestone-3/Module-15/JS-loops/for.js
@@ -0,0 +1,21 @@
+let num = 1; // loop variable
+while (num < 5) { // loop condition
+    console.log(num); // loop statement
+    num++; // loop variable change
+}
+
+// syntax for-loop --> for(initialization; condition; increment/decrement)
+
+for (let num = 1; num < 5; num++) {
+    console.log(num);
+}
+
+// even
+for (let num = 0; num < 20; num=num+2) {
+    console.log(num);
+}
+
+// odd
+for (let num = 1; num < 20; num=num+2) {
+    console.log(num);
+}
