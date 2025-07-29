3e9c76d97f1b47516b18a84bfed754d3df00d763
Antor-Chowdhury <antor1347@gmail.com>
2025-07-29 21:31:30 +0600

    added while.js in module-15

---
+++ Milestone-3/Module-15/JS-loops/while.js
@@ -0,0 +1,15 @@
+const number = 7;
+
+if (number < 10) {
+    console.log('smaller number');
+}
+console.log('Outer loop is getting executed.');
+
+
+// --------------WHILE-----LOOP----------
+let num = 0;
+// const num = 0; // loop variable
+while(num < 5) {
+    console.log('looooping', num);
+    num++; // num = num + 1; or num += 1;
+}
