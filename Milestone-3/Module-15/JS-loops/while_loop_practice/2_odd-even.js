30df84efb1c15ca41ab457a4337eb55f34c36996
Antor-Chowdhury <antor1347@gmail.com>
2025-07-29 22:21:52 +0600

    added 2_odd-even.js in module-15

---
+++ Milestone-3/Module-15/JS-loops/while_loop_practice/2_odd-even.js
@@ -0,0 +1,26 @@
+// Find all the odd numbers from 61 to 100
+
+let num1 = 61;
+
+while(num1 <=  100) {
+    console.log(num1);
+    
+    if(num1 % 2 !== 0){
+        console.log('Odd:', num1);
+    }
+    num1++;
+}
+
+
+// Find all the even numbers from 78 to 98.
+
+let num2 = 78;
+
+while(num2 <=  98) {
+    console.log(num2);
+    
+    if(num2 % 2 === 0){
+        console.log('Even:', num2);
+    }
+    num2++;
+}
