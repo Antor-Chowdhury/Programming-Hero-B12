6345c68052370d3fa8430e47230ad647c610523a
Antor-Chowdhury <antor1347@gmail.com>
2025-07-30 22:40:26 +0600

    added do-while.js in module-15

--- Milestone-3/Module-15/JS-loops/continue.js
+++ Milestone-3/Module-15/JS-loops/continue.js
@@ -1,12 +1,12 @@
 // continue --> skip rest of the code for this iteration
 // break --> done with this loop
 
-// for (let i = 1; i <= 10; i++) {
-//   if (i % 2 !== 0) {
-//     continue;
-//   }
-//   console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 !== 0) {
+    continue;
+  }
+  console.log(i);
+}
 
 // while
 
