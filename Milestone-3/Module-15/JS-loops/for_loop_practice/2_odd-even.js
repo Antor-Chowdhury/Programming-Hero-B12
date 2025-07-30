865a5f19010497b4cdf5ca13997b6f1a6ee181c9
Antor-Chowdhury <antor1347@gmail.com>
2025-07-30 21:21:53 +0600

    added for-loop problem solution

---
+++ Milestone-3/Module-15/JS-loops/for_loop_practice/2_odd-even.js
@@ -0,0 +1,15 @@
+//  subtask-1
+
+for(i = 61; i<=100; i++) {
+    if(i % 2 !==0) {
+        console.log(i);
+    }
+}
+
+// subtask-2
+
+for(i = 78; i<=98; i++) {
+    if(i % 2 ===0) {
+        console.log(i);
+    }
+}
---
+++ Milestone-3/Module-15/JS-loops/for_loop_practice/3_sum-odd.js
@@ -0,0 +1,23 @@
+// subtask-1
+
+let sum = 0;
+for(i = 91; i<=129; i++) {
+    if(i % 2 !==0) {
+        // console.log(i);
+        sum = sum + i;
+    }
+    // console.log(sum);
+}
+console.log(sum);
+
+// subtask-2
+
+let sum1 = 0;
+for(i = 51; i<=85; i++) {
+    if(i % 2 ===0) {
+        // console.log(i);
+        sum1 = sum1 + i;
+    }
+    // console.log(sum);
+}
+console.log(sum1);
