0e4af33af10e0569c08a24e17e18afc207bd3b3d
Antor-Chowdhury <antor1347@gmail.com>
2025-07-30 22:18:11 +0600

    added continue.js in module-15

---
+++ Milestone-3/Module-15/JS-loops/break.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i <= 15; i++) {
+//   console.log(i);
+//   if (i >= 10) {
+//     break;
+//   }
+// }
+
+let num = 54;
+
+while (num > 25) {
+  if (num < 50) {
+    break;
+  }
+  console.log(num);
+  num--;
+}
