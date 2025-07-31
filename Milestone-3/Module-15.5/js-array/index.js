26167d849bdce2bf0b899caaaff6711e3c66350b
Antor-Chowdhury <antor1347@gmail.com>
2025-07-31 22:41:18 +0600

    added index.js and length .js in module-15.5

---
+++ Milestone-3/Module-15.5/js-array/index.js
@@ -0,0 +1,14 @@
+const numbers = [12, 444, 45, 54, 66, 36];
+
+console.log(numbers);
+
+// get element value by index
+console.log(numbers[2]);
+console.log(numbers[5]);
+
+// set or update elements value by index
+
+const abc = [424, 44, 4466, 566, 2375];
+console.log(abc);
+abc[1] = 420;
+console.log(abc);
