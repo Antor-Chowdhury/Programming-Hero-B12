bca543bdf13cec353a18efe42fa8eeabad9d92ce
Antor-Chowdhury <antor1347@gmail.com>
2025-07-31 21:43:57 +0600

    added 2_skip-five.js in continue folder

---
+++ Milestone-3/Module-15/JS-loops/countinue_practice/2_skip-five.js
@@ -0,0 +1,16 @@
+// display odd number from 55 to 85 and skip the numbers divisible by 5.
+
+let num = 55;
+
+while (num <= 85) {
+  if (num % 5 === 0) {
+    num++;
+    continue;
+  }
+
+  if (num % 2 !== 0) {
+    console.log("Odd number:", num);
+  }
+
+  num++;
+}
