0b3c1ad47f2cc27091f44c2a03667dc17a71b540
Antor-Chowdhury <antor1347@gmail.com>
2025-07-31 22:19:42 +0600

    added a readme file in array-problem folder

---
+++ Milestone-3/Module-15.5/js-array/array.js
@@ -0,0 +1,16 @@
+// const num1 = 12;
+// const num2 = 20;
+// const num3 = 44;
+// const num4 = 335;
+
+const numbers = [12, 22, 54, 65];
+console.log(numbers);
+
+const friends = ["Ana", "Antor", "John", "Dimple"];
+console.log(friends);
+
+const passed = [true, false, true, true];
+console.log(passed);
+
+const mixedArray = [12, "antor", true, 23.333];
+console.log(mixedArray);
