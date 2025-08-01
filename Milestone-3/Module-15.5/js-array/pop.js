0263506e87e61ec68aaa2e5a5601d74c07835acd
Antor-Chowdhury <antor1347@gmail.com>
2025-08-01 19:32:08 +0600

    added pop.js in module-15.5

---
+++ Milestone-3/Module-15.5/js-array/pop.js
@@ -0,0 +1,12 @@
+const friends = ["salam", "rohim", "korim", "gelam", "pailam", "jailam"];
+
+// friends.push("Borkot");
+console.log(friends);
+
+const out1 = friends.pop();
+console.log(friends);
+
+const out2 = friends.pop();
+console.log(friends);
+
+console.log(out1, out2);
