109f1447791c3198a41dca1ab9892d792af44a8a
Antor-Chowdhury <antor1347@gmail.com>
2025-08-01 20:48:40 +0600

    added 1-problem.js in module-15.5

---
+++ Milestone-3/Module-15.5/array-practice-solution/1-prolem.js
@@ -0,0 +1,6 @@
+const fruits = ["apple", "orange", "mango", "jackfruit", "watermelon"];
+
+console.log(fruits); // initial values
+console.log(fruits[3]);
+fruits[2] = "jambura";
+console.log(fruits);
