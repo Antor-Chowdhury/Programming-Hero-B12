bb18a026f1159459d8ff9a11b57413ffd5ba0fb6
Antor-Chowdhury <antor1347@gmail.com>
2025-08-01 22:17:41 +0600

    added string.js in module-16

---
+++ Milestone-3/Module-16/js-string-object/string.js
@@ -0,0 +1,23 @@
+const country = "Bangladesh";
+const division = "Rajshahi";
+const district = `Sirajgonj`;
+// const thana = new String("Kamarkhand");
+
+// console.log(typeof country);
+// console.log(typeof division);
+// console.log(typeof district);
+// console.log(typeof thana);
+// console.log(thana);
+
+const numbers = [12, 13, 15, 53, 545];
+// console.log(numbers.length);
+// console.log(numbers[2]);
+
+// array is mutable --> can change array elements
+// string is immutable --> not changeable
+const capital = "Dhaka";
+console.log(capital);
+// console.log(capital.length);
+// console.log(capital[2]);
+capital[1] = "q";
+console.log(capital);
