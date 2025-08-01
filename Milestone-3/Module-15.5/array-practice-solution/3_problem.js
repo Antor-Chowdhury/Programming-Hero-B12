28907d72ad25c2c0cd0813d7c63f058efb023d7c
Antor-Chowdhury <antor1347@gmail.com>
2025-08-01 20:57:22 +0600

    add 3_problem.js in module-15.5

---
+++ Milestone-3/Module-15.5/array-practice-solution/3_problem.js
@@ -0,0 +1,9 @@
+const bookName = ["C++", "Java", "C", "Python", "JavaScript"];
+
+console.log(bookName.includes("JavaScript")); // checking if the javaScript is in the book name or not
+
+if (bookName.includes("JavaScript")) {
+  console.log("Book is available");
+} else {
+  console.log("Book is not found");
+}
