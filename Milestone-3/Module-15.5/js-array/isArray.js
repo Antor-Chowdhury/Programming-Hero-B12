7ace8bfa4764dd826affee0b911ce0c7210e0783
Antor-Chowdhury <antor1347@gmail.com>
2025-08-01 20:05:47 +0600

    added isArray.js in module-15.5

---
+++ Milestone-3/Module-15.5/js-array/isArray.js
@@ -0,0 +1,11 @@
+const friends = ["salam", "rohim", "korim", "gelam", "pailam", "jailam"];
+
+const num = [];
+
+const food = "ros o gol l a";
+const age = 23;
+
+console.log(Array.isArray(friends));
+console.log(Array.isArray(num));
+console.log(Array.isArray(food));
+console.log(Array.isArray(age));
