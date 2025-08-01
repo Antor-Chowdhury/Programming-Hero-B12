f3a223903779256cff7684c20deccd427ee5d592
Antor-Chowdhury <antor1347@gmail.com>
2025-08-01 19:43:18 +0600

    add shift-unshift.js in module-15.5

---
+++ Milestone-3/Module-15.5/js-array/shift-unshift.js
@@ -0,0 +1,8 @@
+const friends = ["salam", "rohim", "korim", "gelam", "pailam", "jailam"];
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+friends.unshift("ghumailam");
+console.log(friends);
