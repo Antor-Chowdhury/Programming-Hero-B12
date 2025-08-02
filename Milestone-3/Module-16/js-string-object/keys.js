3d77fe918f3d788bf9d6c3243e680593b2e07b48
Antor-Chowdhury <antor1347@gmail.com>
2025-08-02 21:33:05 +0600

    add keys.js and values.js in js-string-object

---
+++ Milestone-3/Module-16/js-string-object/keys.js
@@ -0,0 +1,9 @@
+const computer = {
+  brand: "Asus",
+  price: 80000,
+  processor: "amd",
+  ssd: "1tb",
+};
+
+const keys = Object.keys(computer);
+console.log(keys);
---
+++ Milestone-3/Module-16/js-string-object/values.js
@@ -0,0 +1,9 @@
+const computer = {
+  brand: "Asus",
+  price: 80000,
+  processor: "amd",
+  ssd: "1tb",
+};
+
+const values = Object.values(computer);
+console.log(values);
