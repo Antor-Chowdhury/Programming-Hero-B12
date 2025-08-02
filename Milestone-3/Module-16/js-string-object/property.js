1e5fed8f7baddf32cac2c1fff1272062e1500818
Antor-Chowdhury <antor1347@gmail.com>
2025-08-02 21:02:43 +0600

    add property.js in js-string-object

---
+++ Milestone-3/Module-16/js-string-object/property.js
@@ -0,0 +1,22 @@
+const person = {
+  name: "sodor uddin",
+  age: 25,
+  profession: "Developer",
+  salary: 30000,
+  married: true,
+  "fav places": ["bandorban", "saintmartion", "kuakata"], // can't access with dot notation but can be done with []
+};
+
+// console.log(person);
+
+// dot notation
+// console.log(person.profession);
+const income = person.salary;
+// console.log(income);
+
+// third bracket
+console.log(person["age"]);
+const boyos = person["age"];
+console.log(boyos);
+
+console.log(person["fav places"]);
