616957e5185b9501c170dffdb224bfc7f9760945
Antor-Chowdhury <antor1347@gmail.com>
2025-08-02 19:53:47 +0600

    add object.js in js-string-object

---
+++ Milestone-3/Module-16/js-string-object/object.js
@@ -0,0 +1,35 @@
+// primitive
+const age = 25;
+const school = "rifle square";
+const isPassed = true;
+// let isDeveloper
+
+const subjects = ["Bangla", "English", "Physics", "Math"];
+const bottle_wrong = ["white", 45];
+const bottleColor = "white";
+const bottlePrice = 45;
+const bottleDrinks = "water";
+
+// object
+
+// Non-primitive
+const bottle = {
+  brand: "apple",
+  price: 45,
+  color: "white",
+  isClean: true,
+};
+
+const subject = {
+  name: "Biology",
+  teacher: "rasheda mam",
+  examDate: "30 march",
+  chapters: ["first", "second", "third"],
+  exams: {
+    name: "final exam",
+    date: "22 march",
+  },
+};
+
+console.log(subject);
+console.log(subject.chapters);
