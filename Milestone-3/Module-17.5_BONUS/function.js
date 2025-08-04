67991a01ccffa815d6c6c6f93017d495ec8fbfcd
Antor-Chowdhury <antor1347@gmail.com>
2025-08-04 20:30:12 +0600

    add some more code in function.js

--- Milestone-3/Module-17.5_BONUS/function.js
+++ Milestone-3/Module-17.5_BONUS/function.js
@@ -9,10 +9,35 @@ function moneyBag() {
 }
 moneyBag();
 
-function calculator(input1, input2) {
+// addition
+function addition(input1, input2) {
   let sum = input1 + input2;
-  console.log(sum);
+  // console.log(sum);
   return sum; // run without return and with return to see the difference.
 }
-const result = calculator(10, 12);
+const result = addition(10, 12);
 console.log(result);
+
+// subtraction
+function subtraction(input1, input2) {
+  let sub = input1 - input2;
+  return sub;
+}
+const sub = subtraction(20, 10);
+console.log(sub);
+
+// multiplication
+function multiplication(input1, input2) {
+  let multi = input1 * input2;
+  return multi;
+}
+const multi = multiplication(20, 10);
+console.log(multi);
+
+// division
+function division(input1, input2) {
+  let div = input1 / input2;
+  return div;
+}
+const div = division(20, 10);
+console.log(div);
