645200db78f7bdb9ef23ab4affac0a53cf69b33a
Antor-Chowdhury <antor1347@gmail.com>
2025-08-06 21:02:44 +0600

    add array-looping.js in Module-18

---
+++ Milestone-4/Module-18/Js-array-more/array-looping.js
@@ -0,0 +1,28 @@
+const fruits = ["Apple", "Banana", "Anar", "Watermelon", "Coconut"];
+// console.log(fruits[i]);
+// console.log(fruits.length);
+
+// for (let i = 0; i < fruits.length; i++) {
+//   //   console.log(i);
+//   //   console.log(fruits[i]);
+// }
+
+const numbers = [10, 122, 45, 887, 98, 1000];
+for (let i = 0; i < numbers.length; i++) {
+  //   console.log(numbers[i]);
+}
+
+// while
+
+let k = 0;
+while (k < fruits.length) {
+  //   console.log(fruits[k]);
+  k++;
+}
+
+const prices = [15, 880, 49, 78, 25];
+let n = 0;
+while (n < prices.length) {
+  console.log(prices[n]);
+  n++;
+}
