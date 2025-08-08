7ba26d5396f6b95eed130fd56d44a6a47aab6fa1
Antor-Chowdhury <antor1347@gmail.com>
2025-08-08 19:42:59 +0600

    add funtion.js in JS-Function in Module-19

---
+++ Milestone-4/Module-19/JS-Function/function.js
@@ -0,0 +1,25 @@
+const price = 45;
+
+// ** Function Syntax
+
+// function functionName(parameter1, parameter2) {
+//   // code to be executed
+//   return value; // Optional: return a value.
+// }
+
+function fanOffKor() {
+  console.log("bosa theke uthe dara");
+  console.log("Walk towards the switch");
+  console.log("Click the switch to turn off the fan");
+}
+
+function brushTeeth() {
+  console.log("pick up the brush");
+  console.log("add paste");
+  console.log("brush ur teeth for 2 minutes");
+}
+
+//call the function
+
+fanOffKor();
+brushTeeth();
