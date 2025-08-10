8199f017fbd36a30df74a8b8110dc33f12b87fc8
Antor-Chowdhury <antor1347@gmail.com>
2025-08-11 01:43:59 +0600

    add math.js in module-20

---
+++ Milestone-4/Module-20/js-problems-part-1/math.js
@@ -0,0 +1,31 @@
+/***
+ * Below code is a example of JavaScript inbuilt function which is written as -->(Math.min)
+ */
+
+// const Math = {
+//   min: function min(num1, num2) {
+//     return num2;
+//   },
+// };
+
+// console.log(Math.min);
+// console.log(Math.max);
+
+const min = Math.min(45, 21, 65, 99, 126, 5, -10);
+console.log(min);
+
+const max = Math.max(45, 21, 65, 99, 126, 5, -10);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-88));
+console.log(Math.round(2.2));
+console.log(Math.round(2.73));
+console.log(Math.floor(2.78));
+console.log(Math.ceil(2.12));
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
