3fb340c8cf9d16a8e98bd283de3910b60c24db8d
Antor-Chowdhury <antor1347@gmail.com>
2025-08-10 21:46:52 +0600

    add convert.js in js-problems-part-1 in module-20

---
+++ Milestone-4/Module-20/js-problems-part-1/convert.js
@@ -0,0 +1,38 @@
+// 12 inch = 1 feet
+
+function inchToFeet(inch) {
+  const feet = inch / 12;
+  return feet;
+}
+
+const shuvoHeight = inchToFeet(75);
+console.log(shuvoHeight);
+
+// output will be 6 feet 3 inch --> below
+
+function inchToFeet2(inch) {
+  const feetFraction = inch / 12;
+  const feetNumber = parseInt(feetFraction);
+  const inchRemaining = inch % 12;
+  const result = feetNumber + " feet " + inchRemaining + " inches.";
+  return result;
+}
+
+const shuvoHeight2 = inchToFeet2(75);
+console.log(shuvoHeight2);
+
+// km to mile
+function kiloMeterToMiles(kilo) {
+  const mile = kilo * 0.621371;
+  return mile;
+}
+const totalMile = kiloMeterToMiles(120);
+console.log(totalMile.toFixed(2), "Mile");
+
+// mile to km
+function miletoKiloMeter(mile) {
+  const kilo = mile * 1.60934;
+  return kilo;
+}
+const totaKilo = miletoKiloMeter(140);
+console.log(totaKilo.toFixed(2), "Kilometer");
