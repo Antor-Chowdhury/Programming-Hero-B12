294ec17f4109b11ca64db22bb6c6c752373beab2
Antor-Chowdhury <antor1347@gmail.com>
2025-08-19 22:24:42 +0600

    add more js file

---
+++ Milestone-5/Module-24/tour_of_DOM/js/child.js
@@ -0,0 +1,13 @@
+// document.getElementById('players-container').childNodes[3].childNodes[1].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode
+
+// 1st step: create element and set innerText or innerHTML
+
+const newChild = document.createElement("li");
+newChild.innerText = "new baby is born";
+
+// 2nd step: find the parent where you will add the child
+
+const playersList = document.getElementById("player-list");
+
+// 3rd step: append the child to the parent
+playersList.appendChild(newChild);
