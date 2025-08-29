a1f75080c644e70012fe925b2868dedbe7bc04bf
Antor-Chowdhury <antor1347@gmail.com>
2025-08-30 01:27:29 +0600

    add let-const.js in Module-29

---
+++ Milestone-6/Module-29/es6-intro/let-const.js
@@ -0,0 +1,29 @@
+// es6
+
+// console.log(name); // can't show name before initialization
+
+// console.log("device value:", device);
+// var device = "laptop";
+
+const name = "Bangladesh";
+const countryName = "BD" + name;
+console.log(countryName);
+
+let price = 120;
+price = 150;
+console.log(price);
+
+//
+const charge = 50;
+// charge = 45;
+
+const dress = ["shirt", "pant", "jacket"];
+dress.push("maflar");
+console.log(dress);
+
+const student = {
+  name: "jagdul",
+  marks: 50,
+};
+student.marks = 60;
+console.log(student);
