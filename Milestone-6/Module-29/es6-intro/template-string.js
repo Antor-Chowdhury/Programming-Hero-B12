01f9b82973430ef909e42caac992519278f44367
Antor-Chowdhury <antor1347@gmail.com>
2025-08-30 14:00:08 +0600

    add template-string.js in es6-intro

---
+++ Milestone-6/Module-29/es6-intro/template-string.js
@@ -0,0 +1,20 @@
+// const poem = 'amm pata jora jora
+// marbo chabuk chorbe ghora'
+
+// const poem = `Amm pata jora jora
+// marbo chabuk chorbe ghora
+// ore babu ghure dara
+// asche amr pagla ghora`;
+// console.log(poem);
+
+function sum(num1, num2) {
+  const result = num1 + num2;
+  const output = `sum of ${num1} and ${num2} is ${result}`;
+  console.log(output);
+}
+
+sum(10, 20);
+
+const price = 100;
+const discountedPrice = `Discounted price after 20% discount is ${price * 0.8}`;
+console.log(discountedPrice);
