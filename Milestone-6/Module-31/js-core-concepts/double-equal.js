e0d976fdc47d702547323136ffc1cfae5bc54878
Antor-Chowdhury <antor1347@gmail.com>
2025-09-02 13:38:40 +0600

    added triple-equal and double-equal in js-core-concept

---
+++ Milestone-6/Module-31/js-core-concepts/double-equal.js
@@ -0,0 +1,41 @@
+// type coercion (type convert)
+
+/**
+ * if one value is string, another is number: converts string --> number then compare. --> only for ==
+ *
+ *
+ * if one is boolean and another is number. converts boolean to number. if true --> 1 , if false --> 0
+ *
+ *
+ */
+
+// console.log(2 == 2);
+
+// console.log(2 == "2"); // true
+// console.log(1 == "1");
+
+// console.log(1 === 2);
+
+// console.log(true == 1); // true
+// console.log(false == 0);
+
+// console.log(true == "1"); // 1 == '1' --> 1 == 1 --> returns true
+// console.log(false == "0"); // 0 == '0' --> 0 == 0 --> true
+
+// console.log(null == undefined); // true
+
+// console.log(NaN === NaN); // false
+// console.log(NaN == NaN); // false
+
+// console.log(0 === "");
+
+// console.log([] == ""); // true
+// console.log([5] == "5"); // true
+
+// console.log([5].toString()); // converts the array to string and then compare it with "5"
+
+// console.log({} == {}); // false because they are not primitive
+
+// console.log([] == []); // false because they are not primitive
+
+// console.log([5] == [5]); // false , reference is different
---
+++ Milestone-6/Module-31/js-core-concepts/triple-equal.js
@@ -0,0 +1,13 @@
+// === first check the type. If type if different, they are different.
+// if type is same then check the value, if value is same --> true, if value is false --> false
+
+console.log(2 === 2);
+
+console.log(2 === "2");
+
+console.log(1 === 2);
+console.log(true === 2);
+console.log(false === 2);
+console.log(null === undefined);
+console.log(0 === "");
+console.log([] === "");
