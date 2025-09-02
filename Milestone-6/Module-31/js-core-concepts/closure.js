89eab52f01cf43b60d9c30eb046545c68eea11b6
Antor-Chowdhury <antor1347@gmail.com>
2025-09-02 22:27:34 +0600

    add closure.js in js-core-concepts

---
+++ Milestone-6/Module-31/js-core-concepts/closure.js
@@ -0,0 +1,41 @@
+// function counter() {
+//   let count = 0;
+//   function innerFunction() {
+//     console.log("inside the inner function, some one called me");
+//   }
+//   return innerFunction;
+// }
+
+// const output = counter();
+// console.log(output());
+
+function counter() {
+  let count = 10;
+
+  return function (person) {
+    count = count + 1;
+    console.log("inside the inner function", person, count);
+  };
+}
+
+// const innerFunc = counter();
+// innerFunc();
+// innerFunc();
+// innerFunc();
+
+const rahimCounter = counter();
+rahimCounter("rahim");
+rahimCounter("rahim");
+rahimCounter("rahim");
+rahimCounter("rahim");
+
+console.log("-------------------------");
+
+const karimCounter = counter();
+karimCounter("karim");
+
+rahimCounter("rahim");
+
+karimCounter("karim");
+karimCounter("karim");
+karimCounter("karim");
