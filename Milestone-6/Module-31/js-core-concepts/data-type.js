caeb2a3b9407c6f1b8a0623a24c1e3f16f70380a
Antor-Chowdhury <antor1347@gmail.com>
2025-09-02 11:36:36 +0600

    add data-type.js in js-core-concepts

---
+++ Milestone-6/Module-31/js-core-concepts/data-type.js
@@ -0,0 +1,31 @@
+// JS is a dynamic typed programming language
+let data = 4;
+data = "tormuj";
+data = false;
+data = {
+  name: "acid",
+  price: 40,
+};
+data = [445];
+data = undefined;
+data = null;
+console.log(typeof data);
+
+// js: primitive data
+
+const price = 50;
+const name = "bodrul";
+const isPoor = false;
+const merit = null;
+let isKnown = undefined; // usually you should not set value to undefined.
+let isLocal;
+console.log(isKnown, isLocal);
+
+// non-primitive data type
+
+const student = {
+  name: "badal",
+  location: "dhaka",
+  subject: "math",
+};
+const friends = ["tom", "bob", "jack", "rock"];
