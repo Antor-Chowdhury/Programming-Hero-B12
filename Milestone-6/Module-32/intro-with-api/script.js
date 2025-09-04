f97143799d4e37c6bc55613413aa6f208af43db4
Antor-Chowdhury <antor1347@gmail.com>
2025-09-04 22:48:53 +0600

    modified the script.js

--- Milestone-6/Module-32/intro-with-api/script.js
+++ Milestone-6/Module-32/intro-with-api/script.js
@@ -1,1 +1,22 @@
 console.log("Explore API");
+
+const person = {
+  name: "selim",
+  fruit: "dalim",
+  dish: "halim",
+  friends: ["alim", "kolim", "lamim"],
+  isRich: false,
+  money: 34000,
+};
+
+console.log(person, typeof person);
+
+// JSON --> JS object with Notation
+// JSON.stringify --> object to JSON
+// JSON.parse -->  JSON to Object
+
+const personJSON = JSON.stringify(person);
+console.log(personJSON, typeof personJSON);
+
+const parseJSON = JSON.parse(personJSON);
+console.log(parseJSON, typeof parseJSON);
