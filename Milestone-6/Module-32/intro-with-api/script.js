a2821cf86381b096137f6e25f5753a3ec2fac7b9
Antor-Chowdhury <antor1347@gmail.com>
2025-09-05 01:41:14 +0600

    modified script.js

--- Milestone-6/Module-32/intro-with-api/script.js
+++ Milestone-6/Module-32/intro-with-api/script.js
@@ -1,7 +1,24 @@
 const loadData = () => {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
+  fetch("https://jsonplaceholder.typicode.com/todos/1") // promise of response
+    .then((response) => response.json()) // promise of json data
     .then((data) => console.log(data));
+};
+
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      displayPost(data);
+    });
+};
+
+const displayPost = (posts) => {
+  posts.forEach((post) => {
+    console.log(post);
+  });
 };
 
 // console.log("Explore API");
