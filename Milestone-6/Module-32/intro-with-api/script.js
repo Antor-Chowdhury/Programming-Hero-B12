2b8c85d00a08a4ccb526231c2ea9718cd7fb6359
Antor-Chowdhury <antor1347@gmail.com>
2025-09-05 01:07:12 +0600

    modified script.js in intro-with-api

--- Milestone-6/Module-32/intro-with-api/script.js
+++ Milestone-6/Module-32/intro-with-api/script.js
@@ -1,22 +1,28 @@
-console.log("Explore API");
-
-const person = {
-  name: "selim",
-  fruit: "dalim",
-  dish: "halim",
-  friends: ["alim", "kolim", "lamim"],
-  isRich: false,
-  money: 34000,
+const loadData = () => {
+  fetch("https://jsonplaceholder.typicode.com/todos/1")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 };
 
-console.log(person, typeof person);
+// console.log("Explore API");
 
-// JSON --> JS object with Notation
-// JSON.stringify --> object to JSON
-// JSON.parse -->  JSON to Object
+// const person = {
+//   name: "selim",
+//   fruit: "dalim",
+//   dish: "halim",
+//   friends: ["alim", "kolim", "lamim"],
+//   isRich: false,
+//   money: 34000,
+// };
 
-const personJSON = JSON.stringify(person);
-console.log(personJSON, typeof personJSON);
+// console.log(person, typeof person);
 
-const parseJSON = JSON.parse(personJSON);
-console.log(parseJSON, typeof parseJSON);
+// // JSON --> JS object with Notation
+// // JSON.stringify --> object to JSON
+// // JSON.parse -->  JSON to Object
+
+// const personJSON = JSON.stringify(person);
+// console.log(personJSON, typeof personJSON);
+
+// const parseJSON = JSON.parse(personJSON);
+// console.log(parseJSON, typeof parseJSON);
