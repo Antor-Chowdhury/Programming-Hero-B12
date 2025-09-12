f06bfcd7390881206c7dad666b819407d67ac725
Antor-Chowdhury <antor1347@gmail.com>
2025-09-12 22:20:22 +0600

    add index.html in module-34.5

--- Milestone-6/Module-34.5/more-js-concepts/async.js
+++ Milestone-6/Module-34.5/more-js-concepts/async.js
@@ -2,7 +2,16 @@
 
 console.log("one");
 console.log("two");
-callThree();
+
+// setTimeout(callThree, 5000);
+setTimeout(() => {
+  console.log("new three inside set time out");
+}, 4000);
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
 console.log("four");
 console.log("five");
 
