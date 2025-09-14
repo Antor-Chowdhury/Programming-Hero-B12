b39d7c01489bad541b80ef284719b9214907f7de
Antor-Chowdhury <antor1347@gmail.com>
2025-09-15 01:33:36 +0600

    updated cart.js

--- Milestone-6/Module-34.6/browser-storage-api/js/cart.js
+++ Milestone-6/Module-34.6/browser-storage-api/js/cart.js
@@ -2,7 +2,7 @@ const handleAddProduct = () => {
   const productEl = document.getElementById("product");
   const quantityEl = document.getElementById("quantity");
   const product = productEl.value;
-  const quantity = quantityEl.value;
+  const quantity = parseInt(quantityEl.value);
 
   //   console.log("product add", product, quantity);
 
@@ -14,15 +14,30 @@ const handleAddProduct = () => {
 };
 
 const getCart = () => {
-  const cart = {};
+  let cart = {};
+
+  const cartJSON = localStorage.getItem("cart");
+  //   console.log(cartJSON);
+  if (cartJSON) {
+    cart = JSON.parse(cartJSON);
+  }
 
   return cart;
 };
 
 const addProductToCart = (product, quantity) => {
   const cart = getCart();
-  cart[product] = quantity;
+
+  if (cart[product]) {
+    cart[product] = cart[product] + quantity;
+  } else {
+    cart[product] = quantity;
+  }
+
   console.log("cart", cart);
+
+  const cartJSON = JSON.stringify(cart);
+  localStorage.setItem("cart", cartJSON);
 };
 
 const displayProduct = (product, quantity) => {
@@ -34,3 +49,30 @@ const displayProduct = (product, quantity) => {
   const ul = document.getElementById("products-container");
   ul.appendChild(li);
 };
+
+// display products from stored local storage
+
+const displayStoredProducts = () => {
+  const cart = getCart();
+  for (const product in cart) {
+    const quantity = cart[product];
+    console.log(product, quantity);
+    displayProduct(product, quantity);
+  }
+};
+
+displayStoredProducts();
+
+/**
+ * To save Object/array in the Local storage
+ * 1. convert the object to JSON string by using JSON.stringify
+ * 2. localstorage.setItem()
+ *
+ */
+
+/**
+ * to get object/array from the local storage
+ * 1. get the item from the local storage and it will be in JSON string
+ * 2. convert the JSON string to js object by using JSON.parse
+ *
+ */
