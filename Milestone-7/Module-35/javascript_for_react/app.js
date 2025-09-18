728c9449baed4dceec18144e3b9a0b7198ae3b9e
Antor-Chowdhury <antor1347@gmail.com>
2025-09-19 01:09:38 +0600

    add app.html & app.js in module-35

---
+++ Milestone-7/Module-35/javascript_for_react/app.js
@@ -0,0 +1,54 @@
+// variable
+
+// let and const
+
+// let name = "ANTOR";
+// name = "ANTOR CHOWDHURY";
+
+// const countryName = "BD";
+// countryName = "";
+
+// if-else
+
+// const num = 40;
+
+// if (num >= 20 && num <= 30) {
+//   console.log("yes");
+// } else {
+//   console.log("NO");
+// }
+
+const friends = ["HERO", "SRK", "JRK", "RAHIM"];
+
+// friends.push("BD");
+// friends.pop();
+// friends.unshift("BD");
+// friends.shift();
+
+// console.log(friends.slice(2, 3));
+
+// console.log(friends.splice(2, 3));
+
+// for (let i = 0; i < friends.length; i++) {
+//   const element = friends[i];
+//   console.log(element);
+// }
+
+// function sum(a, b) {
+//   console.log(...arguments);
+//   const result = a + b;
+//   return result;
+// }
+
+// const output = sum(3, 5);
+// console.log(output);
+
+// const person = {
+//   name: "Antor",
+//   age: 25,
+//   friends: friends,
+// };
+
+// person.name = "Durjoy";
+
+// console.log(person);
