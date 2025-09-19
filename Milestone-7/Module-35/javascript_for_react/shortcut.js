18811f0b64638d2d718d0d3de209eefbbdee1f78
Antor-Chowdhury <antor1347@gmail.com>
2025-09-20 00:48:26 +0600

    add shortcut.js in Module-35

---
+++ Milestone-7/Module-35/javascript_for_react/shortcut.js
@@ -0,0 +1,37 @@
+// false value
+// 0 -0 "" false null undefined
+
+// "sdfdas" 334 {} [] true ---> truthy
+
+// const test = 1;
+
+// if (test) {
+//   console.log("it's truthy");
+// } else {
+//   console.log("it's falsy");
+// }
+
+// const test = 1;
+
+// test ? console.log("it's truthy") : console.log("it's falsy");
+
+// const num = 50;
+
+// const result = num >= 10 && num <= 20 ? "truee" : "false";
+
+// console.log(result);
+
+// const isActive = true;
+
+// const showUser = () => console.log("show user");
+
+// const hidUser = () => console.log("Hide user");
+
+// // isActive ? showUser() : hidUser();
+
+// isActive && showUser();
+
+// isActive || hidUser();
+
+// let sum = "10";
+// console.log(+num); // will convert it into a number
