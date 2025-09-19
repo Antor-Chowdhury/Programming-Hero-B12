2f2ee4e619bffe86a138cde8ef4cb3ef73ea88e0
Antor-Chowdhury <antor1347@gmail.com>
2025-09-19 14:02:36 +0600

    add array-method.js in module-35

---
+++ Milestone-7/Module-35/javascript_for_react/array-method.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: 1,
+    name: "Redmi",
+    brand: "xiaomi",
+    price: 3000,
+    color: "black",
+  },
+  {
+    id: 2,
+    name: "S24 Ultra",
+    brand: "Samsung",
+    price: 5000,
+    color: "white",
+  },
+  {
+    id: 3,
+    name: "Iphone 12",
+    brand: "Apple",
+    price: 4000,
+    color: "black",
+  },
+  {
+    id: 4,
+    name: "Iphone 14 Pro",
+    brand: "Apple",
+    price: 6000,
+    color: "Blue",
+  },
+  {
+    id: 5,
+    name: "Redmi Note 14",
+    brand: "xiaomi",
+    price: 4500,
+    color: "black",
+  },
+];
+
+// const result = products.map((product) => product.brand);
+// console.log(result);
+
+// products.forEach((product) => console.log(product.price));
+
+// const result = products.filter((product) => product.price >= 5000);
+// console.log(result);
+
+// const result = products.filter((product) => product.id !== 3); // filtering by using unique ID.
+// console.log(result);
+
+// const result = products.find((product) => product.name === "Iphone 12");
+// console.log(result);
--- Milestone-7/Module-35/javascript_for_react/template.js
+++ Milestone-7/Module-35/javascript_for_react/template.js
@@ -13,8 +13,8 @@
 
 // sum(1, 2, 3);
 
-const numbers = [1, 2, 3, 4, 5, 6];
+// const numbers = [1, 2, 3, 4, 5, 6];
 
-const newNumbers = [...numbers, 9, 10];
+// const newNumbers = [...numbers, 9, 10];
 
-console.log(newNumbers);
+// console.log(newNumbers);
