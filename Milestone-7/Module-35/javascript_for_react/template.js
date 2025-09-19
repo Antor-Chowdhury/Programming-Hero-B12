5f125d38ac3d94b2d541902efc067d797c1b8614
Antor-Chowdhury <antor1347@gmail.com>
2025-09-19 13:32:19 +0600

    add template.js in module-35

---
+++ Milestone-7/Module-35/javascript_for_react/template.js
@@ -0,0 +1,20 @@
+// const firstName = "Antor";
+// const lastName = "Chowdhury";
+
+// console.log(`I am ${firstName} ${lastName}`);
+
+// function sum() {
+//   console.log(arguments);
+// }
+
+// const sum = (...rest) => {
+//   console.log(rest);
+// };
+
+// sum(1, 2, 3);
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const newNumbers = [...numbers, 9, 10];
+
+console.log(newNumbers);
