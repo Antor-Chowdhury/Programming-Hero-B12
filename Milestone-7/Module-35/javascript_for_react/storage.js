90a92c001e21a89c171862b208a7cb96a896907a
Antor-Chowdhury <antor1347@gmail.com>
2025-09-20 01:17:01 +0600

    add storage.js in Module-35

---
+++ Milestone-7/Module-35/javascript_for_react/storage.js
@@ -0,0 +1,18 @@
+// const handleAddToStorage = () => {
+//   const name = document.getElementById("name").value;
+//   //   console.log(name);
+//   const id = document.getElementById("id").value;
+//   //   localStorage.setItem(id, name);
+
+//   const data = { id, name };
+//   console.log(data);
+
+//   localStorage.setItem(id, JSON.stringify(data));
+// };
+
+// const storedItem = localStorage.getItem("1234");
+// console.log(JSON.parse(storedItem));
+
+// const clearLocalStorage = () => {
+//   localStorage.clear();
+// };
