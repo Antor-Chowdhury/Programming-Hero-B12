a379ae943c6d644c7c9452827146f8ae692080f6
Antor-Chowdhury <antor1347@gmail.com>
2025-09-20 14:40:47 +0600

    small change in app.jsx

--- Milestone-7/Module-36/explore-core-react-part-1/src/App.jsx
+++ Milestone-7/Module-36/explore-core-react-part-1/src/App.jsx
@@ -1,9 +1,6 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
       <h1>Vite + React</h1>
@@ -13,32 +10,35 @@ function App() {
       <Person></Person>
       <Person></Person>
       <Sports></Sports>
-
     </>
-  )
+  );
 }
 
-function Person () {
-  const age =25;
-  const name = 'Antor'
-  return(
-    <p>I am {name}  & my age is {age}</p>
-  )
+function Person() {
+  const age = 25;
+  const name = "Antor";
+  return (
+    <p>
+      I am {name} & my age is {age}
+    </p>
+  );
 }
 
-function Pet () {
-  return(
-    <p>Dog</p>
-  )
+function Pet() {
+  return <p>Dog</p>;
+}
+
+function Hobby() {
+  return <h3>Still trying to find it.</h3>;
 }
 
 function Sports() {
-  return(
+  return (
     <div>
       <h2>Playing Cricket</h2>
       <p>Playing and Losing</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
