b28ef8014c7dcd04aea1075ae2c5238e3cc31033
Antor-Chowdhury <antor1347@gmail.com>
2025-09-21 14:17:41 +0600

    changed App.jsx in Module-37

--- Milestone-7/Module-37/react-core-concepts-part-2/src/App.jsx
+++ Milestone-7/Module-37/react-core-concepts-part-2/src/App.jsx
@@ -1,9 +1,46 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("I am Clicked");
+  }
+
+  const handleClick3 = () => {
+    alert("Clicked 3");
+  };
+
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  };
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h3>Vite + React</h3>
+      {/* <button onclick="handleClick()">Click Me</button> */}
+
+      <button onClick={handleClick}>Click Me</button>
+
+      <button
+        onClick={function handleClick2() {
+          alert("Clicked 2");
+        }}
+      >
+        Click Me 2
+      </button>
+
+      <button onClick={handleClick3}>Click Me 3</button>
+
+      {/* using direct arrow function in onClick */}
+      <button
+        onClick={() => {
+          alert("Clicked 4");
+        }}
+      >
+        Click Me 4
+      </button>
+
+      <button onClick={() => handleAdd5(10)}>Click Add 5</button>
     </>
   );
 }
