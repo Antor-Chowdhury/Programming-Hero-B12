ae8a522e6c596f53b2d2622cdb6d086f8b681b71
Antor-Chowdhury <antor1347@gmail.com>
2025-09-28 20:47:01 +0600

    add Country.jsx in Module-38

--- Milestone-7/Module-38/react-world-on-the-go/src/components/Countries/Countries.jsx
+++ Milestone-7/Module-38/react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -5,13 +5,13 @@ import Country from "../Country/Country";
 const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
-  console.log(countries);
+  // console.log(countries);
 
   return (
     <div>
       <h1>In the Countries {countries.length}</h1>
       {countries.map((country) => (
-        <Country></Country>
+        <Country key={country.cca3.cca3} country={country}></Country>
       ))}
     </div>
   );
--- Milestone-7/Module-38/react-world-on-the-go/src/components/Country/Country.jsx
+++ Milestone-7/Module-38/react-world-on-the-go/src/components/Country/Country.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-const Country = () => {
+const Country = ({ country }) => {
+  //   console.log(country?.population?.population);
   return (
     <div>
-      <h3>Name: </h3>
+      <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+      <p>
+        <strong>Population:</strong> {country.population.population}
+      </p>
+      <p>
+        <strong>Capital:</strong> {country.capital.capital}
+      </p>
+      <p>
+        <strong>Region: </strong>
+        {country.region.region}
+      </p>
+      <p>
+        <strong>Continents: </strong>
+        {country.continents.continents}
+      </p>
     </div>
   );
 };
