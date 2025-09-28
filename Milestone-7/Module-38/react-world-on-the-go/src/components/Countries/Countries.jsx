86dc2788ab313ca0e5501585c98abb2ad88dbc36
Antor-Chowdhury <antor1347@gmail.com>
2025-09-28 20:16:13 +0600

    add Countries.jsx & Country.jsx in Module-38

--- Milestone-7/Module-38/react-world-on-the-go/src/components/Countries/Countries.jsx
+++ Milestone-7/Module-38/react-world-on-the-go/src/components/Countries/Countries.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { use } from "react";
 
-const Countries = () => {
+import Country from "../Country/Country";
+
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+  console.log(countries);
+
   return (
     <div>
-      <h1>In the Countries</h1>
+      <h1>In the Countries {countries.length}</h1>
+      {countries.map((country) => (
+        <Country></Country>
+      ))}
     </div>
   );
 };
