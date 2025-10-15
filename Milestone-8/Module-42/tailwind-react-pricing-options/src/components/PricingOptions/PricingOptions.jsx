1ea53a6cb98287393601263279ee19823b692f55
Antor-Chowdhury <antor1347@gmail.com>
2025-10-16 01:31:30 +0600

    add PricingOptions card

--- Milestone-8/Module-42/tailwind-react-pricing-options/src/components/PricingOptions/PricingOptions.jsx
+++ Milestone-8/Module-42/tailwind-react-pricing-options/src/components/PricingOptions/PricingOptions.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { use } from "react";
+import PricingCard from "../PricingCard/PricingCard";
 
-const PricingOptions = () => {
-  return <div></div>;
+const PricingOptions = ({ pricingPromise }) => {
+  const pricingData = use(pricingPromise);
+  //   console.log(pricingData);
+
+  return (
+    <div>
+      <h2 className="text-5xl my-8 text-cyan-400 text-center">
+        Get our Membership
+      </h2>
+      <div className="grid md:grid-cols-3 gap-6 p-4">
+        {pricingData.map((pricing) => (
+          <PricingCard key={pricing.id} pricing={pricing}></PricingCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PricingOptions;
