732b6f1f6d41db9094e8a8a6a8442f781a9a2d83
Antor-Chowdhury <antor1347@gmail.com>
2025-10-16 21:48:55 +0600

    added NavLink to the project

---
+++ Milestone-8/Module-43/intro-to-react-router/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Thank you for visiting our website.</p>
+      <a href="/">Terms</a>
+      <a href="/">Privacy</a>
+      <a href="/">Facebook</a>
+    </div>
+  );
+};
+
+export default Footer;
