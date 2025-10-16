8ee949c8e3017aeeb151bebd3f74b6c4dfcda399
Antor-Chowdhury <antor1347@gmail.com>
2025-10-17 02:51:13 +0600

    add dynamic route

--- Milestone-8/Module-43/intro-to-react-router/src/components/UserDetails/UserDetails.jsx
+++ Milestone-8/Module-43/intro-to-react-router/src/components/UserDetails/UserDetails.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useLoaderData } from "react-router";
 
 const UserDetails = () => {
+  const user = useLoaderData();
+  // console.log(user);
+
+  const { website, name } = user;
+
   return (
     <div>
-      <h3>User Details Here</h3>
+      <h2>User Details Here</h2>
+      <h3>Name: {name}</h3>
+      <p>Website: {website}</p>
     </div>
   );
 };
--- Milestone-8/Module-43/intro-to-react-router/src/main.jsx
+++ Milestone-8/Module-43/intro-to-react-router/src/main.jsx
@@ -40,7 +40,7 @@ const router = createBrowserRouter([
       {
         path: "users/:userId",
         loader: ({ params }) =>
-          fetch("https://jsonplaceholder.typicode.com/users"),
+          fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
         Component: UserDetails,
       },
     ],
