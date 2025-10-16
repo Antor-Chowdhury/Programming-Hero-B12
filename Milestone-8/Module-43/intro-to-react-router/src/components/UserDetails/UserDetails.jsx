e018e17cecf39afbc08cd1ff6a36c5b5ef8a6143
Antor-Chowdhury <antor1347@gmail.com>
2025-10-17 03:29:35 +0600

    used userNavigate

--- Milestone-8/Module-43/intro-to-react-router/src/components/UserDetails/UserDetails.jsx
+++ Milestone-8/Module-43/intro-to-react-router/src/components/UserDetails/UserDetails.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { useLoaderData } from "react-router";
+import { useLoaderData, useParams } from "react-router";
 
 const UserDetails = () => {
   const user = useLoaderData();
   // console.log(user);
+
+  const { userId } = useParams();
+  console.log(userId);
+
+  // const params = useParams();
+  // console.log(params.userId);
 
   const { website, name } = user;
 
