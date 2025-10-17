237ac0cdf5a88df3e157f8069011ccea79443dea
Antor-Chowdhury <antor1347@gmail.com>
2025-10-18 02:53:57 +0600

    added Family Tree

---
+++ Milestone-8/Module-44/explore-react-form/src/components/FamilyTree/Aunt.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Cousin from "./Cousin";
+
+const Aunt = () => {
+  return (
+    <div>
+      <h3>Aunt</h3>
+      <section className="flex">
+        <Cousin name="Luffyy"></Cousin>
+        <Cousin name="Aceee"></Cousin>
+      </section>
+    </div>
+  );
+};
+
+export default Aunt;
