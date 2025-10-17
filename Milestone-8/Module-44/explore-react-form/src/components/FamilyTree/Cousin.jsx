2033bfc571ca7071a5576d5a97cad6346beb21d0
Antor-Chowdhury <antor1347@gmail.com>
2025-10-18 04:15:26 +0600

    more context api

--- Milestone-8/Module-44/explore-react-form/src/components/FamilyTree/Cousin.jsx
+++ Milestone-8/Module-44/explore-react-form/src/components/FamilyTree/Cousin.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Special from "./Special";
+import Friend from "./Friend";
 
 const Cousin = ({ name, asset }) => {
   return (
     <div>
       <h3>{name}</h3>
       {name === "Luffyy" && <Special asset={asset}></Special>}
+
+      {name === "Aceee" && <Friend></Friend>}
     </div>
   );
 };
