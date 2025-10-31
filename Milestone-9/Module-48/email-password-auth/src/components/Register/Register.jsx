2585790addc22911385b4bde79e47f97c94dc125
Antor-Chowdhury <antor1347@gmail.com>
2025-10-31 22:18:12 +0600

    added toggle password show in Register

--- Milestone-9/Module-48/email-password-auth/src/components/Register/Register.jsx
+++ Milestone-9/Module-48/email-password-auth/src/components/Register/Register.jsx
@@ -1,10 +1,12 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase/firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const Register = () => {
   const [error, setError] = useState("");
   const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleRegister = (event) => {
     event.preventDefault();
@@ -58,6 +60,11 @@ const Register = () => {
       });
   };
 
+  const handleTogglePasswordShow = (event) => {
+    event.preventDefault();
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -76,12 +83,26 @@ const Register = () => {
                   placeholder="Email"
                 />
                 <label className="label">Password</label>
-                <input
-                  name="password"
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
+                <div className="relative">
+                  <input
+                    name="password"
+                    type={showPassword ? "text" : "password"}
+                    className="input"
+                    placeholder="Password"
+                  />
+                  <button
+                    onClick={handleTogglePasswordShow}
+                    className="btn btn-xs absolute top-2 right-6"
+                  >
+                    {showPassword ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+                  </button>
+                </div>
+                <div>
+                  <label class="label">
+                    <input type="checkbox" class="checkbox" />
+                    Accept Our Terms and Conditions.
+                  </label>
+                </div>
                 <div>
                   <a className="link link-hover">Forgot password?</a>
                 </div>
