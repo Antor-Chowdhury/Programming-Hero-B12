62abade55c59f8a8704a734f9001a77c2c2ee9bd
Antor-Chowdhury <antor1347@gmail.com>
2025-11-04 01:52:52 +0600

    added auth context

---
+++ Milestone-9/Module-49/react-firebase-auth-integration/src/contexts/AuthContext/AuthProvider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AuthContext } from "./AuthContext";
+
+const AuthProvider = ({ children }) => {
+  const authInfo = {
+    email: "allu@arjun.com",
+  };
+
+  return <AuthContext value={authInfo}>{children}</AuthContext>;
+};
+
+export default AuthProvider;
