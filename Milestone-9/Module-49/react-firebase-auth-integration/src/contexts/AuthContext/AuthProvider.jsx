e5ee4b9565ac820ad8b9128f1adf30bf13c48e91
Antor-Chowdhury <antor1347@gmail.com>
2025-11-04 04:00:23 +0600

    added useEffect to control the observer

--- Milestone-9/Module-49/react-firebase-auth-integration/src/contexts/AuthContext/AuthProvider.jsx
+++ Milestone-9/Module-49/react-firebase-auth-integration/src/contexts/AuthContext/AuthProvider.jsx
@@ -1,12 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../../firebase/firebase.init";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  // For Register
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // For SignIn
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword((auth, email, password));
+  };
+
+  //   useEffect(() => {}, []);
+  //   useEffect(() => {
+  //     // step -> 1: observer set
+  //     // step -> 2: set in a variable
+  //     // step -> 3: return and call the variable so that you can clear the reference.
+  //   }, []);
+
+  useEffect(() => {
+    // set the observer
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      console.log("current user in auth state change", currentUser);
+
+      setUser(currentUser);
+    });
+    // clear the observer on unmount
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  //   get current user info
+  //   onAuthStateChanged(auth, (currentUser) => {
+  //     if (currentUser) {
+  //       console.log("inside observer: if", currentUser);
+  //     } else {
+  //       console.log("inside observer: if", currentUser);
+  //     }
+  //   });
+
   const authInfo = {
-    email: "allu@arjun.com",
+    // createUser: createUser ;  --> can be written like this also.
+    user,
+    createUser,
+    signInUser,
   };
 
   return <AuthContext value={authInfo}>{children}</AuthContext>;
 };
 
 export default AuthProvider;
+
+/**
+ * 1. create a context and export
+ *
+ * 2. create a provider --> so that you can reuse the context in different places
+ *
+ * 3. ensure you use the children prop
+ *
+ * 4. make sure use the auth provider in the router.
+ *
+ * 5. make the create user with email and password shared via provider.
+ *
+ * 6. set createUser in the authInfo object to share via context.
+ */
