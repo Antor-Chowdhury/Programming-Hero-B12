3a99385ad1be0b939057710eb6520665856ac595
Antor-Chowdhury <antor1347@gmail.com>
2025-11-03 21:44:12 +0600

    added Registration

--- Milestone-9/Module-49/react-firebase-auth-integration/src/components/Register/Register.jsx
+++ Milestone-9/Module-49/react-firebase-auth-integration/src/components/Register/Register.jsx
@@ -1,9 +1,74 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React from "react";
+import { Link } from "react-router";
+import { auth } from "../../firebase/firebase.init";
 
 const Register = () => {
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    console.log("form submitted", name, email, password);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
-    <div>
-      <h2>This is Register.</h2>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-20">
+      <div className="card-body">
+        <h1 className="text-4xl font-bold">Please Register!</h1>
+        <form onSubmit={handleRegister}>
+          <fieldset className="fieldset">
+            {/* name field */}
+            <label className="label">Name</label>
+            <input
+              name="name"
+              type="text"
+              className="input"
+              placeholder="Name"
+            />
+
+            {/* email field */}
+            <label className="label">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="input"
+              placeholder="Email"
+            />
+
+            {/* password filed */}
+            <label className="label">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-neutral mt-4">Register</button>
+          </fieldset>
+        </form>
+        <p>
+          Already have an account? Please{" "}
+          <Link
+            className="text-blue-500 font-medium hover:text-purple-600 underline"
+            to={"/login"}
+          >
+            Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
